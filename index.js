41a14c52d06e545da4c01a27dcaaa8cc90fa2a69
Walter Panacci <150435336+walterpanacci@users.noreply.github.com>
2024-03-13 10:07:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,8 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/whoami", function(req, res) {
+  res.json({"ipaddress": req.socket.remoteAddress, "language": req.headers["accept-language"], "software": req.headers["user-agent"]})
+}
+);
